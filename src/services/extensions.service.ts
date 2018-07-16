cab478503202f4784ec348a10fa77d949e627a5c
Ben Stein <ben@jammerware.com>
2018-07-16 18:53:32 -0400

    Updated API with new endpoints.

--- src/services/extensions.service.ts
+++ src/services/extensions.service.ts
@@ -2,25 +2,15 @@ import { NcExtension } from '@nucard/models/dist';
 
 export class ExtensionsService {
     private _extensions: NcExtension[] = [
-        // {
-        //     id: '5fa3f8ed-04ee-4f8b-a888-621b14383128',
-        //     name: "Jammerware's Wild 'n' Crazy MtG Extension",
-        //     apiVersion: { major: 1, minor: 0, patch: 0 },
-        //     extensionVersion: { major: 1, minor: 0, patch: 0 },
-        //     queryEndpoint: "https://nucard-mtg.herokuapp.com/cards/query",
-        //     randomCardEndpoint: "https://nucard-mtg.herokuapp.com/cards/random",
-        //     raritiesEndpoint: "https://nucard-mtg.herokuapp.com/rarities",
-        //     rulesSymbolsEndpoint: "https://nucard-mtg.herokuapp.com/rules-symbols",
-        // },
         {
             id: 'c059d135-d4b6-493a-b98e-0eba0583d86e',
             name: "Jammerware's A:NR",
             apiVersion: { major: 1, minor: 0, patch: 0 },
             extensionVersion: { major: 1, minor: 0, patch: 0 },
-            queryEndpoint: "https://nucard-netrunner.herokuapp.com/cards/search",
+            externalProvidersEndpoint: "https://nucard-netrunner.herokuapp.com/external-providers",
             randomCardEndpoint: "https://nucard-netrunner.herokuapp.com/cards/random",
-            raritiesEndpoint: "https://nucard-netrunner.herokuapp.com/rarities",
             rulesSymbolsEndpoint: "https://nucard-netrunner.herokuapp.com/rules-symbols",
+            searchEndpoint: "https://nucard-netrunner.herokuapp.com/cards/search",
         },
     ];
 
