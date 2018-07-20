f17faa92b500fbfddd289ae9ea6300c454ba1e36
Ben Stein <ben@jammerware.com>
2018-07-20 14:05:02 -0400

    Updated version of models and added logos for existing extensions.

--- src/services/extensions.service.ts
+++ src/services/extensions.service.ts
@@ -5,6 +5,7 @@ export class ExtensionsService {
         {
             id: 'c059d135-d4b6-493a-b98e-0eba0583d86e',
             name: "Jammerware's A:NR",
+            logo: "https://imgur.com/hTdlsRN.png",
             gameName: "Android: Netrunner",
             apiVersion: { major: 1, minor: 0, patch: 0 },
             extensionVersion: { major: 1, minor: 0, patch: 0 },
@@ -20,6 +21,7 @@ export class ExtensionsService {
         {
             id: '177bf6a0-3eec-412c-b114-cf9cc47c295e',
             name: "Jammerware's Hearthstone",
+            logo: "https://i.imgur.com/BOYXukS.png",
             gameName: "Hearthstone",
             apiVersion: { major: 1, minor: 0, patch: 0 },
             extensionVersion: { major: 1, minor: 0, patch: 0 },
